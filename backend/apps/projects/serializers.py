20c6a5d29b52cfcafdfdc82255fd3f15d68c0f69
BrandonGarate177 <brandongarate177@gmail.com>
2025-05-29 15:35:27 -0700

    Added Folder serializer to project

--- backend/apps/projects/serializers.py
+++ backend/apps/projects/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Project
-from apps.files.serializers import FileSerializer
-from apps.files.models import File
+from apps.files.serializers import FileSerializer, FolderSerializer
+from apps.files.models import File, Folder
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     files = FileSerializer(many=True, read_only=True)
-    
+    folders = FolderSerializer(many=True, read_only=True)
+
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description', 'owner', 'files', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'description', 'owner', 'files', 'folders', 'created_at', 'updated_at']
         read_only_fields = ['owner']
